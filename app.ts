7a48039d6be4d7b117d93b2051666a005a128e53
Michael O'Donnell <gorillaejones@gmail.com>
2019-12-03 14:00:27 -0600

    object examples added

--- app.ts
+++ app.ts
@@ -1,6 +1,18 @@
-const person = {
+//can make an object a generic typescript object with : object
+// const person: object = {
+//     name: 'Michael',
+//     age: 34
+// };
+
+//typescript object with typed keys looks thusly, though this is bad practice:
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+    //good practice
+    const person = {
     name: 'Michael',
     age: 34
 };
 
-console.log(person.nickname);
+console.log(person.name);

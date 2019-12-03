7dd72da62f17f79599f8cde762e619e1169e6230
Michael O'Donnell <gorillaejones@gmail.com>
2019-12-03 12:37:30 -0600

    adds more examples to app.ts

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-function addinator(numberOne, numberTwo) {
-    if (typeof numberOne !== 'number' || typeof numberTwo !== 'number') {
-        throw new Error('Not a number!!');
+function addinator(numberOne, numberTwo, showResult) {
+    //javascript way you could throw an error if inputs are not numbers
+    //   if (typeof numberOne !== "number" || typeof numberTwo !== "number") {
+    //     throw new Error("Not a number!!");
+    //   }
+    if (showResult) {
+        console.log(numberOne + numberTwo);
     }
-    console.log(typeof numberOne);
-    return numberOne + numberTwo;
+    else {
+        return numberOne + numberTwo;
+    }
 }
 //if one input is a string, turns the return into a string, turns 2nd int into string
 //unless the function inputs above are changed so they have to be a number using typescript
 //change to number below, no more error when compiled
-var numberOne = '5';
+//all numbers are floats
+var numberOne = 5;
 var numberTwo = 3.45;
-var result = addinator(numberOne, numberTwo);
-console.log(result);
+var printResult = true;
+addinator(numberOne, numberTwo, printResult);

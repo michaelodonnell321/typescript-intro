d633bad16bc3173c08fef2aa5e3c15aca57e63ed
Michael O'Donnell <gorillaejones@gmail.com>
2019-12-12 10:37:15 -0600

    combined types examples added

--- app.js
+++ app.js
@@ -1,55 +1,16 @@
-//can make an object a generic typescript object with : object
-// const person: object = {
-//     name: 'Michael',
-//     age: 34
-// };
-//typescript object with typed keys looks thusly, though this is bad practice:
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
-//good practice
-var person = {
-    name: 'Michael',
-    age: 34,
-    hobbies: ['Reading', 'Music'],
-    role: [2, 'awesome']
-};
-person.role.push('admin');
-person.role[1] = 10;
-var favoriteActivites;
-//this would create an error:
-// favoriteActivites = sports;
-//this would not
-// favoriteActivites = ['Reading', 'Music']
-var bestActivities;
-bestActivities = ['Fun times', 12];
-//any should only be used when needed
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    //can use methods that work on inferred types
-    console.log(hobby.toUpperCase());
-    //map will not work on strings, this would be an error
-    // console.log(hobby.map());
+//combined type, separated with | - either will work now
+function combine(inputOne, inputTwo) {
+    //this can handle numbers and strings with some extra logic
+    var result;
+    if (typeof inputOne === 'number' && typeof inputTwo === 'number') {
+        result = inputOne + inputTwo;
+    }
+    else {
+        result = inputOne.toString() + inputTwo.toString();
+    }
+    return result;
 }
-//nested objects work as you would expect:
-// const product = {
-//     id: 'example',
-//     price: 12.99,
-//     tags: ['great-offer', 'hot-and-new'],
-//     details: {
-//         title: 'Red Carpet',
-//         description: 'A great carpet - almost brand new!'
-//     }
-// }
-//type of the object:
-// {
-//     id: string;
-//     price: number;
-//     tags: string[],
-//     details: {
-//         title: string;
-//         description: string;
-//     }
-// }
-console.log(person.name);
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine('Michael', 'Carolyn');
+console.log(combinedNames);
---
+++ objects-arrays-enums.ts
@@ -0,0 +1,78 @@
+//can make an object a generic typescript object with : object
+// const person: object = {
+//     name: 'Michael',
+//     age: 34
+// };
+
+//using enum for Role, takes on number value
+//so admin is 0, read_only 1, etc
+//can also explicity define, admin = 5, admin = 'admin', etc
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR
+}
+//typescript object with typed keys looks thusly, though this is bad practice:
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+//good practice
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  //a tuple, first element is a number, second element is a string
+  role: [number, string];
+} = {
+  name: "Michael",
+  age: 34,
+  hobbies: ["Reading", "Music"],
+  role: [2, "awesome"]
+};
+
+//this will not work, as this is defined above
+// person.role = [0, 'adimin', 'user'];
+
+let favoriteActivites: string[];
+//this would create an error:
+// favoriteActivites = sports;
+//this would not
+// favoriteActivites = ['Reading', 'Music']
+
+let bestActivities: any[];
+bestActivities = ["Fun times", 12];
+//any should only be used when needed
+
+for (const hobby of person.hobbies) {
+  //can use methods that work on inferred types
+  console.log(hobby.toUpperCase());
+  //map will not work on strings, this would be an error
+  // console.log(hobby.map());
+}
+
+//nested objects work as you would expect:
+
+// const product = {
+//     id: 'example',
+//     price: 12.99,
+//     tags: ['great-offer', 'hot-and-new'],
+//     details: {
+//         title: 'Red Carpet',
+//         description: 'A great carpet - almost brand new!'
+//     }
+// }
+
+//type of the object:
+
+// {
+//     id: string;
+//     price: number;
+//     tags: string[],
+//     details: {
+//         title: string;
+//         description: string;
+//     }
+// }
+
+console.log(person.name);

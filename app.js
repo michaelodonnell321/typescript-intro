5f1cb9d72889dab4a11ee208332735ec3a264f84
Michael O'Donnell <gorillaejones@gmail.com>
2019-12-12 10:48:31 -0600

    adds literal type examples

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-//combined type, separated with | - either will work now
-function combine(inputOne, inputTwo) {
+//union types, separated with | - either will work now
+function combine(inputOne, inputTwo, resultConversion) {
     //this can handle numbers and strings with some extra logic
     var result;
-    if (typeof inputOne === 'number' && typeof inputTwo === 'number') {
-        result = inputOne + inputTwo;
+    if (typeof inputOne === "number" && typeof inputTwo === "number" || resultConversion === 'as_number') {
+        result = +inputOne + +inputTwo;
     }
     else {
         result = inputOne.toString() + inputTwo.toString();
     }
+    //   if (resultConversion === 'as_number') {
+    //       return +result;
+    //   } else {
+    //       return result.toString();
+    //   }
     return result;
 }
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, 'as_number');
 console.log(combinedAges);
-var combinedNames = combine('Michael', 'Carolyn');
+var combinedStringAges = combine('30', '26', 'as_number');
+console.log(combinedStringAges);
+var combinedNames = combine("Michael", "Carolyn", 'as_string');
 console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,17 +1,30 @@
-//combined type, separated with | - either will work now
-function combine(inputOne: number | string, inputTwo: number | string) {
-    //this can handle numbers and strings with some extra logic
-    let result;
-    if (typeof inputOne === 'number' && typeof inputTwo === 'number') {
-        result = inputOne + inputTwo;
-    } else {
-        result = inputOne.toString() + inputTwo.toString();
-    }
-    return result;
+//union types, separated with | - either will work now
+function combine(
+  inputOne: number | string,
+  inputTwo: number | string,
+  //literal types, union type as well, has to be exactly one or the other to be allowed
+  resultConversion: 'as_number' | 'as_string'
+) {
+  //this can handle numbers and strings with some extra logic
+  let result;
+  if (typeof inputOne === "number" && typeof inputTwo === "number" || resultConversion === 'as_number') {
+    result = +inputOne + +inputTwo;
+  } else {
+    result = inputOne.toString() + inputTwo.toString();
+  }
+//   if (resultConversion === 'as_number') {
+//       return +result;
+//   } else {
+//       return result.toString();
+//   }
+  return result;
 }
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, 'as_number');
 console.log(combinedAges);
 
-const combinedNames = combine('Michael', 'Carolyn');
+const combinedStringAges = combine('30', '26', 'as_number');
+console.log(combinedStringAges);
+
+const combinedNames = combine("Michael", "Carolyn", 'as_string');
 console.log(combinedNames);
